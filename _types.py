1a0503d3906c117b1ce3876d5414cc284abe7826
lovian <belovian@gmail.com>
2024-10-08 02:16:44 +0900

    add instance.

---
+++ _types.py
@@ -0,0 +1,81 @@
+from pydantic import BaseModel
+
+
+class Colors:
+  DEFAULT='\033[0m'
+  CYAN='\033[92m'
+  YELLOW='\033[93m'
+  GREY='\033[90m'
+
+class Offer(BaseModel):
+  ID:str
+  CUDA:str
+  N:str
+  Model:str
+  PCIE:str
+  cpu_ghz:str
+  vCPUs:str
+  RAM:str
+  Disk:str
+  price:str
+  DLP:str
+  DLP_usd:str
+  score:str
+  NV_driver:str
+  Net_up:str
+  Net_down:str
+  R:str
+  Max_Days:str
+  mach_id:str
+  status:str
+  ports:str
+  country:str
+
+  def print_summary(self):
+    print(f'{Colors.CYAN}{self.Model} {Colors.YELLOW}${self.price}{Colors.DEFAULT} - {Colors.GREY}{self}{Colors.DEFAULT}')
+    
+
+
+class Instance(BaseModel):
+  ID:str
+  Machine:str
+  Status:str
+  num:str
+  Model:str
+  Util:str
+  vCPUs:str
+  RAM:str
+  Storage:str
+  SSH_Addr:str
+  SSH_Port:str
+  price:str
+  Image:str
+  Net_up:str
+  Net_down:str
+  R:str
+  Label:str
+  age:str
+
+  @classmethod
+  def from_str(cls, line: str) -> 'Instance':
+      fields = line.split(' ')
+      return Instance(
+          ID=fields[0],
+          Machine=fields[1],
+          Status=fields[2],
+          num=fields[3],
+          Model=fields[4],
+          Util=fields[5],
+          vCPUs=fields[6],
+          RAM=fields[7],
+          Storage=fields[8],
+          SSH_Addr=fields[9],
+          SSH_Port=fields[10],
+          price=fields[11],
+          Image=fields[12],
+          Net_up=fields[13],
+          Net_down=fields[14],
+          R=fields[15],
+          Label=fields[16],
+          age=fields[17]
+      )
--- vrl.py
+++ vrl.py
@@ -4,6 +4,8 @@ from typing import List
 from vastai import VastAI
 from pydantic import BaseModel
 
+from _types import Instance, Offer
+
 api_key = None
 home = os.path.expanduser("~")
 with open(f'{home}/.vast_api_key', 'r') as file:
@@ -13,36 +15,13 @@ with open(f'{home}/.vast_api_key', 'r') as file:
 order='-inet_down'
 
 
-class Offers(BaseModel):
-  ID:str
-  CUDA:str
-  N:str
-  Model:str
-  PCIE:str
-  cpu_ghz:str
-  vCPUs:str
-  RAM:str
-  Disk:str
-  price:str
-  DLP:str
-  DLP_usd:str
-  score:str
-  NV_driver:str
-  Net_up:str
-  Net_down:str
-  R:str
-  Max_Days:str
-  mach_id:str
-  status:str
-  ports:str
-  country:str
 
 
-def parse_offers(offers:str)->List[Offers]:
+def parse_offers(offers:str)->List[Offer]:
   result = []
   rows = offers.split('\n')
 
-  keys = list(Offers.model_fields.keys())
+  keys = list(Offer.model_fields.keys())
   for index, _row in enumerate(rows):
     if len(_row) == 0: # last line is empty.
       break
@@ -56,7 +35,7 @@ def parse_offers(offers:str)->List[Offers]:
     obj = {}
     for iter in range(len(items)):
       obj[keys[iter]] = items[iter]
-    obj = Offers(**obj)
+    obj = Offer(**obj)
     result.append(obj)
 
   return result
@@ -71,30 +50,98 @@ def retrieve_gpu_model(gpu_name:str)->str:
       'A100X',
       'A100_PCIE',
     ]
+  else:
+    list = [gpu_name]
 
   return f'[{",".join(list)}]'
 
 
-def vrl(gpu_name:str):
-  api = VastAI(api_key=api_key)
 
-  _gpu_name = retrieve_gpu_model(gpu_name)
+class VRLOptions(BaseModel):
+  title:str # project title,
+  lm_parameter:int # 7B, 11B, ...
+  favor_gpu:str
+  rl_optimization:str # DPO, OROP, ...
+  disk:int
 
-  offer_conditions = [
-    'reliability>0.99',
-  ]
+  
+class VRL():
+  options:VRLOptions
+  api:VastAI
+  selected_offer:Offer
 
-  # 요구하는 gpu ram에 따라 장치를 선택하게 해야함
-  offer_conditions.append('gpu_ram<=80')
-  offer_conditions.append('num_gpus=1')
-  offer_conditions.append(f'gpu_name in {_gpu_name}')
-  offers_str = api.search_offers(query=' '.join(offer_conditions), order=order)
-  offers = parse_offers(offers_str)
+  def __init__(self, options:VRLOptions):
+    self.api = VastAI(api_key=api_key)
+    self.options = options
 
-  for offer in offers:
-    print(f'\033[92m{offer.Model} \033[93m${offer.price}\033[0m - \033[90m{offer}')
+
+
+  def __find_instance(self):
+    _gpu_name = retrieve_gpu_model(self.options.favor_gpu)
+
+    offer_conditions = [
+      'reliability>0.99',
+    ]
+
+    # 요구하는 gpu ram에 따라 장치를 선택하게 해야함
+    offer_conditions.append('gpu_ram>=42')
+    offer_conditions.append('num_gpus=1')
+    offer_conditions.append(f'gpu_name in {_gpu_name}')
+    offers_str = self.api.search_offers(query=' '.join(offer_conditions), order=order)
+    offers = parse_offers(offers_str)
+
+    for offer in offers:
+      offer.print_summary()
+
+    # select first item
+    self.selected_offer = offers[0]
+
+    print("SELECTED OFFER:")
+    self.selected_offer.print_summary()
+
+  def __create_instance(self):
+    self.api.create_instance(
+      ID=self.selected_offer.ID,
+      label=f'VRL#{self.options.title}',
+      disk=self.options.disk,
+      image="vllm/vllm-openai:latest"
+      )
+
+  def train(self):
+    self.__find_instance()
+    #self.__create_instance()
+    instances = self.api.show_instances()
+    print(instances)
 
 
 
 if __name__ == "__main__":
-  vrl('A100')
+
+  lines="""ID        Machine  Status   Num  Model      Util. %  vCPUs    RAM  Storage  SSH Addr      SSH Port  $/hr    Image                    Net up  Net down  R     Label             age(hours)
+13022723  22952    running   1x  A100_SXM4  0.0      32.0   515.6  200      ssh8.vast.ai  22722     1.6400  vllm/vllm-openai:latest  946.8   8766.9    99.5  VRL#susu_dpo_001  2036.33
+  """
+
+  for index, line in enumerate(lines.split('\n')):
+
+    row = re.sub(r'\s+', ' ', line).strip()
+    if index == 0:
+      continue
+    if len(row) == 0:
+      continue
+
+
+    instance = Instance.from_str(row)
+    print(instance)
+  exit(0)
+
+
+
+  options = VRLOptions(
+    title="susu_dpo_001",
+    lm_parameter=7,
+    rl_optimization="DPO",
+    favor_gpu='A100',
+    disk=200,
+  )
+  vrl = VRL(options)
+  vrl.train()
